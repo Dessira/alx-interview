a9630d571291f65f41a710e738fda0bc45f79f37
Desire Barine <desirebarine16@gmail.com>
2022-12-22 22:33:24 +0100

    task 1

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""creates a pascal triangle"""
+
+def pascal_triangle(n):
+    """creates a pascal triangle in list format"""
+    if n == 0:
+        return []
+    if n == 1:
+        return [1]
+    base = [1]
+    n = n - 1
+    count = 0
+    final = [[1]]
+    new_base = []
+    while count < n:
+        new_base.append(1)
+        for i in range(len(base) - 1):
+            new_base.append(base[i] + base[i + 1])
+        new_base.append(1)
+        base = new_base
+        final += [new_base]
+        count += 1
+        new_base = []
+    return final
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+<h1>Pascal triangle using python</h1>
