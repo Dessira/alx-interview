6845e64eb1bf37ae61d168d8a75bb9245817f938
Desire Barine <desirebarine16@gmail.com>
2023-01-13 04:25:19 +0100

    task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+ Minimum Operations
+"""
+
+
+def minOperations(n):
+    """minimum operations required 
+    """
+    d = n // 2
+    while d > 0:
+        q, r = divmod(n, d)
+        if (r == 0):
+            return q + minOperations(d)
+        d -= 1
+    return 0
