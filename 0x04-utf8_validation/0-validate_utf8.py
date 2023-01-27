fd9058f861ea7635333f2488a77bbea1ed3b5c4c
Desire Barine <desirebarine16@gmail.com>
2023-01-27 05:48:12 +0100

    task1"

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Checks if if a given data set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """checks if data represents a valid encoding
+    """
+    holder = []
+    for val in data:
+        holder.append(val)
+    try:
+        bytes(holder).decode()
+    except (ValueError, UnicodeError):
+        return False
+    else:
+        return True
