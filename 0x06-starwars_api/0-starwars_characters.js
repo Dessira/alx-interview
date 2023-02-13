da2141e89f337c477bf6951f5293242d430417bf
Desire Barine <desirebarine16@gmail.com>
2023-02-13 03:54:59 +0100

    task done

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require("request");
+const end = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${end}`;
+request(url, (error, response, body) =>{
+  if (error){
+    return console.log(error);
+  }
+  const res = JSON.parse(body);
+  const stars = res.characters;
+  for (let i = 0; i < stars.length; i++) {
+    request(stars[i], (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
