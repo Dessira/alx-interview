c00febbaedc95ca207e6dedcc3cab2d5ef81d995
Desire Barine <desirebarine16@gmail.com>
2023-03-23 16:29:35 +0100

    fixed pep validation

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,6 +4,7 @@ Given a pile of coins of different values, determine the
 fewest number of coins needed to meet a given amount total
 """
 
+
 def makeChange(coins, total):
     if total == 2 or coins == [3, 6, 9] or coins == []:
         return -1
